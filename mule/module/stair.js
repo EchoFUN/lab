3f827b2ad35b8cbf2cdf15e06543dc02f0d3a39f
xukai <xukai@meituan.com>
2015-12-01 01:31:02 +0800

    优化代码

--- mule/module/stair.js
+++ mule/module/stair.js
@@ -2,11 +2,9 @@
 define(function () {
   'use strict';
 
-  var SERVER_SIDE = '//localhost:3001'; 
-
   return {
     load: function (name, req, onload, config) {
-      req([SERVER_SIDE + '?q=' + name], function (content) {
+      req([config.bridge + '?q=' + name], function (content) {
         onload(content);
       });
     }
